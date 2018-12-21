225a6b8619d86983a818d58360f9ab5faa3ebebd
ayesha54 <akaleem306@gmail.com>
2018-12-21 17:42:09 +0500

    modify type and user type fields as data types integer so that we can use use enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_21_095514) do
+ActiveRecord::Schema.define(version: 2018_12_21_122535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,10 @@ ActiveRecord::Schema.define(version: 2018_12_21_095514) do
     t.string "other_phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "type"
+    t.integer "type"
+    t.integer "user_type"
+    t.string "questmark_reference"
+    t.boolean "questmark_status"
   end
 
   create_table "roles", force: :cascade do |t|
